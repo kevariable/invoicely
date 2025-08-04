fe5505c49242db56a81d902665ff611ecd86eb4e
Kevin Abrar Khansa <kevariable@gmail.com>
2025-08-04 23:54:22 +0700

    Add logo

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -25,12 +25,13 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('admin')
+            ->brandLogo(asset('logo.png'))
             ->topNavigation()
             ->path('admin')
             ->login()
             ->brandLogoHeight('3.5rem')
             ->colors([
-                'primary' => Color::Neutral,
+                'primary' => Color::Orange,
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
